8cc9e395f8bbfd812c96a1dda702c69a1ee73f9b
IZERTIS\josue.pedro <josue.pedro@izertislabs.pt>
2025-03-30 17:09:32 +0100

    first commit and creation of repository

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from './routes';
+import { ThemeProvider } from '@emotion/react';
+import { LightTheme } from './shared/themes';
+import { MenuLateral } from './shared/components/meun-lateral/MenuLateral';
+import { AppDrawerProvider } from './shared/contexts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const App = () => {
+    return (
+        <ThemeProvider theme={LightTheme}>
+            <AppDrawerProvider>
+                <BrowserRouter>
+                    <MenuLateral>
+                        <AppRoutes />
+                    </MenuLateral>
+                </BrowserRouter>
+            </AppDrawerProvider>
+        </ThemeProvider>
+    );
+};
---
+++ src/routes/index.tsx
@@ -0,0 +1,21 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Button } from '@mui/material';
+import { useDrawerContext } from '../shared/contexts';
+
+export const AppRoutes = () => {
+    const { toggleDrawerOpen } = useDrawerContext();
+    return (
+        <Routes>
+            <Route
+                path='/pagina-inicial'
+                element={
+                    <Button variant='contained' color='primary' onClick={toggleDrawerOpen}>
+                        Toggle Drawer
+                    </Button>
+                }
+            />
+
+            <Route path='*' element={<Navigate to={'/pagina-inicial'} />} />
+        </Routes>
+    );
+};
